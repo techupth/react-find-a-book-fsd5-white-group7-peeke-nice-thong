e8e3f3f94ff395298c3bc3ef9fc66a00cc69e2f9
Gravizz <peeke0rr@gmail.com>
2023-12-26 15:55:01 +0700

    feat:add search book function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  return <div className="App">{/* start coding here */}</div>;
-}
+const App = () => {
+  const [text, setText] = useState("");
+  const [books, setBooks] = useState([]);
+
+  const handleInputChange = (event) => {
+    setText(event.target.value);
+    console.log(text);
+  };
+
+  useEffect(() => {
+    Search();
+  }, [text]);
+
+  async function Search() {
+    const result = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${text}`
+    );
+    setBooks(result.data.items);
+  }
+
+  return (
+    <div>
+      <h1>Find a Book</h1>
+      <input type="text" value={text} onChange={handleInputChange} />
+      <div>
+        <ul>
+          {books.map((book) => (
+            <li key={book.id}>
+              <h3>{book.volumeInfo.title}</h3>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default App;
